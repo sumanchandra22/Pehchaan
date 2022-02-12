7ba9e6ce396a838835ad95fdae39fbd2b3297e5b
Anupriya <anujhapriya.2002@gmail.com>
2022-02-12 14:22:40 +0530

    nav

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser"); 
+const ejs = require("ejs");
+
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("static"));
+
+
+
+app.get("/", function(req,res){
+   
+        res.render("home");
+    
+})
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
