5f5049e551b80df9cb548e950320286e161c334f
Suman Chandra <91794723+sumanchandra22@users.noreply.github.com>
2022-02-12 18:45:00 +0530

    Update diff_ladies.js

---
+++ diff_ladies.js
@@ -0,0 +1,152 @@
+exports.getLady = function() {
+    
+    const diffLadies = [
+        {
+            lady_id:1,
+            image : "images/put2",
+            name : "Rekha Devi", 
+            heading:"I am a Mithila Painter From Madhubani, Bihar, India and my speciality is Mithila Painting and I make made posters and mithila painted sarees for weddings and occasions.",
+            para1: "I am 57 years old and a part of Pehchaan since 4 years.In Bihar Mithila painting posters are one of the major part of weddin occasions .My products are been sold outside india too. PrimeMiniter Narendra Modi himself has praised the maithili artculture and has encouraged many other ladies. Some of the girls of our NGO are learning this art from mee  ",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "93498374939",
+            products:{
+                img1: "p_img/mithila1",
+                img2: "p_img/m2",
+                img3: "p_img/m3",
+                img4: "p_img/m4",
+                img5: "p_img/m6",
+                img6: "p_img/m10"
+            }
+        },
+        {
+            lady_id:2,
+            image : "images/lady2.png",
+            name : "Apurva Mittal", 
+            heading:"I am a Handicraft Worker From Nagpur, India and my speciality is Woollen Craft and I make woollen sweaters , mats, child clothing and variety of other products.",
+            para1: "I am 63 years old and a part of Pehchaan since 6 years.My hand knitted woolen crafts are one of the best sellors here.Here we all ladies sit together and makes several products for our customers. There are a lot of activities arranged here for ladies of all age groups.  ",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india .",
+            phone: "9998374939",
+            products:{
+                img1: "p_img/p51.jpeg",
+                img2: "p_img/p52.jpeg",
+                img3: "p_img/p53.jpeg",
+                img4: "p_img/p54.jpeg",
+                img5: "p_img/p55.jpeg",
+                img6: "p_img/p56.jpeg"
+            }
+        },
+
+        {
+            lady_id:2,
+            image : "images/lady7.png",
+            name : "Nidhi Khanna", 
+            heading:"I am from Ujjain, Madhya Pradesh and I like do craft work. Craft of any kind",
+            para1: "Batik Print is popular craft existent in Ujjain and many bed sheets, sarees, wall panels, portraits, dress materials, etc. contain the rich batik prints. At my age i want to inspire other women who want to work and earn by utilizing their talent ",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "7883749392",
+            products:{
+                img1: "p_img/p5.png",
+                img2: "p_img/p7.png",
+                img3: "p_img/p15.png",
+                img4: "p_img/p23.png",
+                img5: "p_img/p26.png",
+                img6: "p_img/p34.png"
+            }
+        },
+
+        {
+            lady_id:2,
+            image : "images/lady4.png",
+            name : "Ruchi Aggarwal", 
+            heading:"I am a CraftsPerson from Maharashtra, India and my speciality is Woollen Craft and Embroidery. I make wall hangings,  pullovers and many more items.",
+            para1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium fuga reprehenderit, quidem blanditiis labore rem? Praesentium corporis, numquam ipsa, laboriosam, dolor minus illo ea et unde eius assumenda culpa! Placeat voluptas rerum id magnam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, voluptatibus?",
+           para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "7883749392",
+            products:{
+                img1: "p_img/p16.png",
+                img2: "p_img/p8.png",
+                img3: "p_img/p17.png",
+                img4: "p_img/p24.png",
+                img5: "p_img/p27.png",
+                img6: "p_img/p30.png"
+            }
+        },
+
+        {
+            lady_id:2,
+            image : "images/lady1.png",
+            name : "Bindu Anand", 
+            heading:"I am a Handicraft worker from Uttar Pradesh, India and my area is Wooden Craft and Clay Work. I make wooden candle stand, dolls and clay stands too.",
+            para1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium fuga reprehenderit, quidem blanditiis labore rem? Praesentium corporis, numquam ipsa, laboriosam, dolor minus illo ea et unde eius assumenda culpa! Placeat voluptas rerum id magnam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, voluptatibus?",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "7883749392",
+            products:{
+                img1: "p_img/p36.png",
+                img2: "p_img/p9.png",
+                img3: "p_img/p18.png",
+                img4: "p_img/p31.png",
+                img5: "p_img/p35.png",
+                img6: "p_img/p43.png"
+            }
+        },
+
+        {
+            lady_id:2,
+            image : "images/lady5.png",
+            name : "Seema Kumari", 
+            heading:"My home is near Banaras. I make small wooden figurines and things out of wool.",
+            para1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium fuga reprehenderit, quidem blanditiis labore rem? Praesentium corporis, numquam ipsa, laboriosam, dolor minus illo ea et unde eius assumenda culpa! Placeat voluptas rerum id magnam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, voluptatibus?",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "7883749392",
+            products:{
+                img1: "p_img/p1.jpg",
+                img2: "p_img/p10.png",
+                img3: "p_img/p19.png",
+                img4: "p_img/p32.png",
+                img5: "p_img/p37.png",
+                img6: "p_img/p42.png"
+            }
+        },
+        
+
+        {
+            lady_id:2,
+            image : "images/lady6.png",
+            name : "Sona Rani", 
+            heading:"I am from small area near Jaipur, Rajasthan. I love making clay pots and stitching to creat various crafts.",
+            para1: "A traditional art form using ‘Rann ki Mitti’, it originates in the village of Khavda, Rann of Kutch. The beautiful ochre / gerua shades give this art form an earthy feel with painted symmetric black and white designs. The special mud with which it is made is reportedly procured from near a lake",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "7883749392",
+            products:{
+                img1: "p_img/p2.jpeg",
+                img2: "p_img/p12.png",
+                img3: "p_img/p20.png",
+                img4: "p_img/p28.png",
+                img5: "p_img/p38.png",
+                img6: "p_img/p40.png"
+            }
+        },
+
+        {
+            lady_id:2,
+            image : "images/lady10",
+            name : "Sunita Kumari", 
+            heading:"I am a Ceramicist and CraftsPerson from Rajasthan, India. I make crockery and bamboo items along with cloth printing and Jute work.",
+            para1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium fuga reprehenderit, quidem blanditiis labore rem? Praesentium corporis, numquam ipsa, laboriosam, dolor minus illo ea et unde eius assumenda culpa! Placeat voluptas rerum id magnam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, voluptatibus?",
+            para2:"Support of our customers have encouraged us to work on this mission to make Pehchaan an identity for all those ladies who have talents but havent got the platform to showcase it.It will be our heartiest request to share this website so that we can reach all women over india.",
+            phone: "7883749392",
+            products:{
+                img1: "p_img/p3.jpg",
+                img2: "p_img/p11.png",
+                img3: "p_img/p21.png",
+                img4: "p_img/p44.png",
+                img5: "p_img/p36.png",
+                img6: "p_img/p29.png"
+            }
+        }
+
+
+    ];
+
+    return diffLadies;
+}
